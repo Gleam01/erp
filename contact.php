be0a617bf6baf51515a49e04c89d02ca9c5a144a
Yassr HADJ <yassrhadj@gmail.com>
2018-12-24 10:40:06 +0100

    Updated successfully

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="FindTech">
     <meta name="keyword" content="">
@@ -11,15 +11,8 @@
 
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
 
-    <!-- Main Stylesheet File -->
-    <link href="css/style1.css" rel="stylesheet">
-
     <!-- Input tel Stylesheet File -->
     <link href="css/phone.css" rel="stylesheet">
-
-    <!-- Responsive Stylesheet File -->
-
-    <link href="css/responsive.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="css/intlTelInput.css">
@@ -27,69 +20,62 @@
     <link href="css/style.css" rel="stylesheet" />
 
     <link href="css/contact.css" rel="stylesheet" />
-    <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-    <!--===============================================================================================-->
-      <link rel="stylesheet" type="text/css" href="css/util.css">
-      <link rel="stylesheet" type="text/css" href="css/main.css">
-    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
   </head>
 
 <body>
   <div class="container-fluid">
-    <header>
-      <nav class="navbar navbar-expand-sm bg-degraded justify-content-end">
-        <div class="navbar-brand">
-          <a class="font-bold-white" href="#">Gérer ses entreprises devient plus simple avec ERP Africa !</a>
+    <!-- Navigation -->
+    <nav class="bg-degraded navbar navbar-expand-sm navbar-light" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Logo</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger font-bold-white" href="http://www.qualitat-group.net/">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link font-bold-white js-scroll-trigger" href="#">Actualités</a>
+            </li>
+          </ul>
         </div>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link font-bold-white" href="http://www.qualitat-group.net/">Accueil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link font-bold-white" href="#">Actualités</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      </div>
+    </nav>
+    <br><br>
 
+    <div class="row">
+      <?php
+        if (isset($_GET['ok'])) {
+          if ($_GET['ok']=="1") { ?>
+            <div class="alert alert-success alert-dismissible d-block w-50 mx-auto">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <p class="success">Votre message a été envoyé. Merci!</p>
+            </div>
+          <?php }
+          elseif ($_GET['ok']=="0") { ?>
+            <div class="alert alert-danger alert-dismissible d-block w-50 mx-auto">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <p class="error">Veuillez bien renseigner tous les champs svp !</p>
+            </div>
+        <?php  }
+        }
+      ?>
+    </div>
 		<div class="wrap-contact100">
 			<form method="post" action="contactform/contactform.php" class="contact100-form validate-form">
 				<span class="contact100-form-title">
 					Laissez nous un message
 				</span>
         
-        <?php
-          if (isset($_GET['ok'])) {
-            if ($_GET['ok']=="1") { ?>
-              <div class="alert alert-success alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                <p class="success">Votre message a été envoyé. Merci!</p>
-              </div>
-            <?php }
-            elseif ($_GET['ok']=="0") { ?>
-              <div class="alert alert-danger alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                <p class="success">Veuillez bien renseigner tous les champs svp !</p>
-              </div>
-          <?php  }
-          }
-        ?>
-
 				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Name is required">
 					<span class="label-input100">Votre nom</span>
 					<input class="input100" type="text" name="lastName">
@@ -133,7 +119,7 @@
     <footer>
       <div class="footer">
           <div class="text-center font-bold-white">
-            <div class="copyright">
+            <div class="copyright text-center font-bold-white">
               &copy; ERP. Tous droits réservés.
           </div>
         </div>
@@ -145,13 +131,13 @@
   </div>
 
   <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 
   <!-- Popper JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="vendor/bootstrap/js/popper.min.js"></script>
 
   <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
   <!-- input tel script -->
   <script src="js/intlTelInput.js"></script>
   <script>
